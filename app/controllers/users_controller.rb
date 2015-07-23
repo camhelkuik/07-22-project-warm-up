64e2db5728fe9a4938a6c98e26b91b698de02656
Cameron Helkuik <cameronhelkuik@gmail.com>
2015-07-23 16:48:15 -0500

    Got the bootstrap layout to be default, need to move things around

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -49,20 +49,6 @@ class UsersController < ApplicationController
     @task = Task.where("user_id" => session[:user_id])
   end
   
-  # def new
-  #   @user = User.new
-  # end
-  #
-  # def create
-  #   @user = User.new(user_params)
-  #
-  #   if @user.save
-  #     redirect_to users_path
-  #   else
-  #     render "new"
-  #   end
-  # end
-  
   def edit
     @user = User.find(session[:user_id])
   end
